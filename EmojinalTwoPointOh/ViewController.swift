76aee9cf210c89ab512beaee6214552fd7c0b8f7
Isabella <ikmaki14@gmail.com>
2020-01-10 12:50:43 -0600

    a work in progress

--- EmojinalTwoPointOh/ViewController.swift
+++ EmojinalTwoPointOh/ViewController.swift
@@ -9,18 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+    
+    let emojis = ["♈️": "🔥🔥", "♉️": "💪💪", "♊️": "👯‍♀️👯‍♀️", "♋️": "😭😭", "♌️": "🌟🌟", "♍️": "🥇🥇", "♎️": "⚖️⚖️", "♏️": "😈😈", "♐️": "🥊🥊", "♑️": "🤑🤑", "♒️": "🦄🦄", "♓️": "👩‍🎨👩‍🎨"];
 
     @IBAction func showMessage(_ sender: UIButton) {
+        
+        let selectedEmotion = sender.titleLabel?.text
         let alertController = UIAlertController(title: "Your horoscope: ", message: "message", preferredStyle: UIAlertController.Style.alert);
         
         alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil));
         
         present(alertController, animated: true, completion: nil);
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
     }
     
 }
